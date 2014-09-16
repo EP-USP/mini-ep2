c661d75737ef3eab6db23f98c649be8e68c7ece9
Ian Carvalho <iancarv@gmail.com>
2014-09-15 23:13:23 -0300

    better html parse

--- teste.py
+++ teste.py
@@ -1,19 +1,26 @@
-import urllib2
+from urllib.request import urlopen
+import html.parser
 import re
 
-bandex_file = urllib2.urlopen('http://www.usp.br/coseas/cardapio.html')
-bandex_html = bandex_file.read()
+def select_html_tag(tag, string):
+	regex = '<' + tag + '[^>]*>(.*?)</' + tag + '[^>]*>'
+	return re.finditer(regex, string, re.DOTALL|re.IGNORECASE)
+
+def get_text(html):
+	lista = re.findall("(?<=>)[^ <][^<]+(?=<)", html)
+	print(lista)
+	l = '\n'.join(lista)
+	l = l.replace('&nbsp;', '')
+	return str(l)
+
+bandex_file = urlopen('http://www.usp.br/coseas/cardapio.html')
+bandex_html = bandex_file.read().decode('latin1')
+# print(bandex_html)
+bandex_html = re.sub('\s{2,}', ' ', bandex_html)
 bandex_file.close()
 
-ugly_list = re.findall("(?<=>)[^<]+(?=<)", bandex_html)
-beautiful_list = []
 
-for item in ugly_list:
-	if (item != '\n'):
-		f = item.replace('\n', ' ')
-		f = re.sub('\s+', ' ', f)
-		if f != ' ':
-			beautiful_list.append(f)
-			
-for item in beautiful_list:
-	print format(item)
+for tr_match in select_html_tag('tr', bandex_html):
+	for td_match in select_html_tag('td', tr_match.group(1)):
+		#get_text(td_match.group(1))
+		print(get_text(td_match.group(1)))
