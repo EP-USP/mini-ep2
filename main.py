c8a4ab66406b186dbfc3e65f65e7685dc745f7b1
Marcelo Rabello Rossi <rabello@usp.br>
2014-09-18 07:53:54 -0300

    Converted all identation to spaces

--- main.py
+++ main.py
@@ -6,46 +6,45 @@ import argparse
 
 def main():
 
-	parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-	parser.add_argument("-b", "--bandex", help="Local")
-	parser.add_argument("-d", "--dia", help="Dia da semana")
-	parser.add_argument("-a", "--almoco", help="Almoço")
-	parser.add_argument("-j", "--janta", help="Janta")
+    parser.add_argument("-b", "--bandex", help="Local")
+    parser.add_argument("-d", "--dia", help="Dia da semana")
+    parser.add_argument("-a", "--almoco", help="Almoço")
+    parser.add_argument("-j", "--janta", help="Janta")
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	raw_menu = generate_raw_menu(args.bandex)
+    raw_menu = generate_raw_menu(args.bandex)
 
 # Gera um cardapio 'bruto' em html para o bandejao escolhido pelo usuario
 def generate_raw_menu(self, bandex):
 
-	if bandex == 'central':
-		url = 'http://www.usp.br/coseas/cardapio.html'
-	elif bandex == 'fisica':
-		url = 'http://www.usp.br/coseas/cardapiofisica.html'
-	elif bandex == 'quimica':
-		url = 'http://www.usp.br/coseas/cardapioquimica.html'
-	else
-		url = 'http://www.usp.br/coseas/cardcocesp.html'
+    if bandex == 'central':
+        url = 'http://www.usp.br/coseas/cardapio.html'
+    elif bandex == 'fisica':
+        url = 'http://www.usp.br/coseas/cardapiofisica.html'
+    elif bandex == 'quimica':
+        url = 'http://www.usp.br/coseas/cardapioquimica.html'
+    else
+        url = 'http://www.usp.br/coseas/cardcocesp.html'
 
-	bandex_file = urlopen(url)
-	bandex_html = bandex_file.read().decode('latin1')
-	bandex_html = re.sub('\s{2,}', ' ', bandex_html)
-	bandex_file.close()
+    bandex_file = urlopen(url)
+    bandex_html = bandex_file.read().decode('latin1')
+    bandex_html = re.sub('\s{2,}', ' ', bandex_html)
+    bandex_file.close()
 
-	return bandex_html
+    return bandex_html
 
 # Gera uma lista, contendo os itens do cardapio da semana, a partir de um cardapio 'bruto'
 def generate_list_menu(self, raw_menu):
 
-	soup = BeautifulSoup(bandex_html)
-	table = soup.table
+    soup = BeautifulSoup(bandex_html)
+    table = soup.table
 
-	list_menu = []
+    list_menu = []
 
-	for string in table.stripped_strings:
-    	list_menu.append(string)
+    for string in table.stripped_strings:
+        list_menu.append(string)
 
     return list_menu
-
