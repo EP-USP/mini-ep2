e382ba5e17a5cfae359e1c42ec57c9df3535665d
Ian Carvalho <iancarv@gmail.com>
2014-09-18 23:47:57 -0300

    quase final

--- menu.py
+++ menu.py
@@ -1,68 +1,74 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+import re
 
 class Menu(object):
-	def __init__(self, d, m):
-		self.day = d
-		self.meal = m
+    def __init__(self, d, m, html):
+        self.day = d
+        self.meal = m
+        self.html = html
 
-	def generate_list_menu(self):
+    def generate_list_menu(self):
 
-		soup = BeautifulSoup(bandex_html)
-		table = soup.table
+        soup = BeautifulSoup(self.html)
+        table = soup.table
 
-		list_menu = []
+        list_menu = []
 
-		for string in table.stripped_strings:
-    		list_menu.append(string)
+        for string in table.stripped_strings:
+            list_menu.append(string)
 
-    	return list_menu
+        return list_menu
 
-	def generate_day_menu(self):
-		self.list_menu = self.generate_list_menu();
-		day_menu = []
+    def generate_day_menu(self):
+        list_menu = self.generate_list_menu();
+        day_menu = []
 
-		if self.day == 'SEGUNDA-FEIRA':
-			init = list_menu.index('SEGUNDA-FEIRA')
-			end = list_menu.index('TERÇA-FEIRA')
-		elif self.day == 'TERÇA-FEIRA':
-			init = list_menu.index('TERÇA-FEIRA')
-			end = list_menu.index('QUARTA-FEIRA')
-		elif self.day == 'QUARTA-FEIRA':
-			init = list_menu.index('QUARTA-FEIRA')
-			end = list_menu.index('QUINTA-FEIRA')
-		elif self.day == 'QUINTA-FEIRA':
-			init = list_menu.index('QUINTA-FEIRA')
-			end = list_menu.index('SEXTA-FEIRA')
-		elif self.day == 'QUINTA-FEIRA':
-			init = list_menu.index('QUINTA-FEIRA')
-			end = list_menu.index('SEXTA-FEIRA')
-		elif self.day == 'SEXTA-FEIRA':
-			init = list_menu.index('SEXTA-FEIRA')
-			end = list_menu.index('SÁBADO')
-		elif self.day == 'SÁBADO':
-			init = list_menu.index('SÁBADO')
-			end = list_menu.index('DOMINGO')
-		else:
-			init = list_menu.index('DOMINGO')
-			end = list_menu[len(list_menu) - 1]
+        if self.day == 'SEGUNDA-FEIRA':
+            init = list_menu.index('SEGUNDA-FEIRA')
+            end = list_menu.index('TERÇA-FEIRA')
+        elif self.day == 'TERÇA-FEIRA':
+            init = list_menu.index('TERÇA-FEIRA')
+            end = list_menu.index('QUARTA-FEIRA')
+        elif self.day == 'QUARTA-FEIRA':
+            init = list_menu.index('QUARTA-FEIRA')
+            end = list_menu.index('QUINTA-FEIRA')
+        elif self.day == 'QUINTA-FEIRA':
+            init = list_menu.index('QUINTA-FEIRA')
+            end = list_menu.index('SEXTA-FEIRA')
+        elif self.day == 'QUINTA-FEIRA':
+            init = list_menu.index('QUINTA-FEIRA')
+            end = list_menu.index('SEXTA-FEIRA')
+        elif self.day == 'SEXTA-FEIRA':
+            init = list_menu.index('SEXTA-FEIRA')
+            end = list_menu.index('SÁBADO')
+        elif self.day == 'SÁBADO':
+            init = list_menu.index('SÁBADO')
+            end = list_menu.index('DOMINGO')
+        else:
+            init = list_menu.index('DOMINGO')
+            end = len(list_menu) - 1
 
-		for i in range(init, end)
-			day_menu.append(list_menu[i])
+        for i in range(init, end):
+            day_menu.append(list_menu[i])
 
-		return day_menu
+        return day_menu
 
-	def generate_meal_menu(self):
-		day_menu = self.generate_day_menu()
-		init_meal = [i for i, x in enumerate(day_menu) if x == self.day]
-		menu = ''
-		if self.meal == 'lunch':
-			for i in range(init_meal):
-				menu += day_menu[i]
-				print(day_menu[i])
-		else:
-			for i in range(init_meal[1], day_menu[len(day_menu) - 1]):
-				menu += day_menu[i]
-				print(day_menu[i])
+    def generate_meal_menu(self):
+        day_menu = self.generate_day_menu()
+        init_meal = [i for i, x in enumerate(day_menu) if x == self.day]
+        menu = ''
+        print(init_meal)
+        if self.meal == 'lunch':
+            meal_range = range(init_meal[0] + 1, init_meal[1])
+        else:
+            meal_range = range(int(init_meal[1]) + 1, len(day_menu))
 
-		return menu
+        for i in meal_range:
+            partial = day_menu[i]
+            partial += '\n'
+            if not partial[0].isupper() and not partial[0].isdigit():
+                menu = menu[:len(menu) - 1]
+            menu +=  partial
+        # menu = re.sub(' / ', '/', menu)
+        return menu
--- restaurante.py
+++ restaurante.py
@@ -1,28 +1,27 @@
 from urllib.request import urlopen
+import re
+from menu import Menu
 
+class Restaurante(object):
+    def __init__(self, nome):
+        self.nome = nome
+    def get_html(self):
+            if self.nome == 'central':
+                url = 'http://www.usp.br/coseas/cardapio.html'
+            elif self.nome == 'fisica':
+                url = 'http://www.usp.br/coseas/cardapiofisica.html'
+            elif self.nome == 'quimica':
+                url = 'http://www.usp.br/coseas/cardapioquimica.html'
+            else:
+                url = 'http://www.usp.br/coseas/cardcocesp.html'
 
-def Restaurante(object):
-	def __init__(self, nome):
-		self.nome = nome
+            bandex_file = urlopen(url)
+            bandex_html = bandex_file.read().decode('latin1')
+            bandex_html = re.sub('\s{2,}', ' ', bandex_html)
+            bandex_file.close()
 
+            return bandex_html
 
-	def get_html(self):
-			if self.nome == 'central':
-	    		url = 'http://www.usp.br/coseas/cardapio.html'
-			elif self.nome == 'fisica':
-	    		url = 'http://www.usp.br/coseas/cardapiofisica.html'
-			elif self.nome == 'quimica':
-	    		url = 'http://www.usp.br/coseas/cardapioquimica.html'
-			else:
-	    		url = 'http://www.usp.br/coseas/cardcocesp.html'
-
-			bandex_file = urlopen(url)
-			bandex_html = bandex_file.read().decode('latin1')
-			bandex_html = re.sub('\s{2,}', ' ', bandex_html)
-			bandex_file.close()
-
-			return bandex_html
-
-	def get_menu(self, d, m):
-		menu = Menu(d, m)
-		print(menu.generate_meal_menu())
+    def get_menu(self, d, m):
+        menu = Menu(d, m, self.get_html())
+        print(menu.generate_meal_menu())
