5108b05479cdedb746119311758130c7ae79d960
Ian Carvalho <iancarv@gmail.com>
2014-09-19 00:05:47 -0300

    used dictionary for restaurante

--- restaurante.py
+++ restaurante.py
@@ -6,21 +6,22 @@ class Restaurante(object):
     def __init__(self, nome):
         self.nome = nome
     def get_html(self):
-            if self.nome == 'central':
-                url = 'http://www.usp.br/coseas/cardapio.html'
-            elif self.nome == 'fisica':
-                url = 'http://www.usp.br/coseas/cardapiofisica.html'
-            elif self.nome == 'quimica':
-                url = 'http://www.usp.br/coseas/cardapioquimica.html'
-            else:
-                url = 'http://www.usp.br/coseas/cardcocesp.html'
+            urls = {'central': 'http://www.usp.br/coseas/cardapio.html',
+                    'fisica': 'http://www.usp.br/coseas/cardapiofisica.html',
+                    'quimica': 'http://www.usp.br/coseas/cardapioquimica.html',
+                    'pco': 'http://www.usp.br/coseas/cardcocesp.html'}
+            try:
+                url = urls[self.nome]
+                bandex_file = urlopen(url)
+                bandex_html = bandex_file.read().decode('latin1')
+                bandex_html = re.sub('\s{2,}', ' ', bandex_html)
+                bandex_file.close()
+                return bandex_html
+            except KeyError:
+                print('Nome de bandeijao invalido')
+            finally:
+                exit()
 
-            bandex_file = urlopen(url)
-            bandex_html = bandex_file.read().decode('latin1')
-            bandex_html = re.sub('\s{2,}', ' ', bandex_html)
-            bandex_file.close()
-
-            return bandex_html
 
     def get_menu(self, d, m):
         menu = Menu(d, m, self.get_html())
