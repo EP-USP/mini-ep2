5fd8e89c3654543bc45a45cee9cd2be2b840ef7f
Marcelo Rabello Rossi <rabello@usp.br>
2014-09-19 00:05:36 -0300

    First attempt to test

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 class Test(object):
 	
+	def compare(self, string1, string2):
+		if string1 == string2:
+			print('O teste passou com sucesso!')
+		else
+			print('O teste falhou! =(')
+
 	def main():
 		
 		mon_lunch_central = 'Arroz/feijão/arroz integral\n\
@@ -31,4 +37,14 @@ class Test(object):
 
 		fri_dinner_pco = 'Fechado'
 
-		rest_central = Restaurante(c)
+		rest_central = Restaurante('central')
+		rest_fisica = Restaurante('fisica')
+		rest_quimica = Restaurante('quimica')
+		rest_pco = Restaurante('pco')
+
+		compare(rest_central.get_menu('segunda', 'almoco'), mon_lunch_central)
+		compare(rest_fisica.get_menu('sabado', 'almoco'), sat_lunch_phys)
+		compare(rest_quimica.get_menu('quarta', 'janta'), wed_dinner_chem)
+		compare(rest_pco.get_menu('sexta', 'almoco'), fri_lunch_pco)
+		compare(rest_pco.get_menu('sexta', 'janta'), fri_dinner_pco)
+
