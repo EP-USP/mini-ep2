57e1d8f5a1619ef633204825769b58e61ffd2a6d
Marcelo Rabello Rossi <rabello@usp.br>
2014-09-18 23:48:17 -0300

    Added test class

---
+++ test.py
@@ -0,0 +1,34 @@
+class Test(object):
+	
+	def main():
+		
+		mon_lunch_central = 'Arroz/feijão/arroz integral\n\
+		Hambúrguer barbecue\n\
+		Creme de milho\n\
+		Salada de escarola\n\
+		Opcional: PVT com legumes\n\
+		Bananinha/refresco\n\
+		Valor calórico de 1 refeição 1055 kcal'
+
+		sat_lunch_phys = 'FECHADO'
+
+		wed_dinner_chem = 'Arroz/Feijão/Arroz integral\n\
+		Peixe ao forno\n\
+		Chuchu na salsa\n\
+		Salada de alface\n\
+		Opcional: Quibe de PVT\n\
+		Mamão\n\
+		Refresco\n\
+		876 kcal'
+
+		fri_lunch_pco = 'Arroz/feijão/arroz integral\n\
+		Tiras de carne à cigana\n\
+		Mandioca corada\n\
+		Salada de almeirão\n\
+		Opcional: Bolinho de PVT ao molho shoyo\n\
+		Maçã/refresco\n\
+		Valor calórico de 1 refeição 829 kcal'
+
+		fri_dinner_pco = 'Fechado'
+
+		rest_central = Restaurante(c)
